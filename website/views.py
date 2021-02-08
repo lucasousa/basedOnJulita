1abff07de2b816937aeb75f160243ed4e46cfced
Lucas Sousa <lucas103101@gmail.com>
2021-02-08 03:15:17 -0300

    listing news on the homepage (home/index)

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from notice.models import Notice
 
 def index(request):
-    return render(request, "website/index.html")
+    notices = Notice.objects.all().order_by('-published_date')[:3]
+    return render(request, "website/index.html", context={"notices": notices})
